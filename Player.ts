d189ee74a44408df45884328e46bf439ceee2c88
Maxime <maxime.attala@edu.devinci.fr>
2022-03-25 17:52:52 +0100

    one player is able to attack another player

--- Player.ts
+++ Player.ts
@@ -1,25 +1,44 @@
 export class Player {
-    name: string;
-    health: number;
-    defense: number;
+    private readonly name: string;
+    private health: number;
+    private readonly defense: number;
+    private readonly attack: number;
 
-    constructor(name, health, defense) {
+    constructor(name, health, defense, attack) {
         this.name = name;
         this.health = health;
         this.defense = defense;
+        this.attack = attack;
     }
 
-    getName() {
+
+    getName(): string {
         return this.name;
     }
 
-    getHealth() {
+    getHealth(): number {
         return this.health;
+    }
+
+    setHealth(health: number): void {
+        this.health = health;
+    }
+
+    getDefense() {
+        return this.defense;
     }
 
     takeDamage(damage) {
         if (damage > this.defense) {
             this.health -= damage - this.defense;
+        }
+    }
+
+    attackPlayer(player: Player): void {
+        let playerHealth = player.getHealth();
+        if (this.attack > player.getDefense()) {
+            let damage = this.attack - player.defense;
+            player.setHealth(playerHealth - damage);
         }
     }
 
--- rpg.test.ts
+++ rpg.test.ts
@@ -10,17 +10,46 @@ import {Player} from "./Player";
 });*/
 
 
-describe('PLAYER TESTS', function () {
+describe('PLAYER STATS', function () {
     it('should die when health gets to 0', function () {
-        let player = new Player("Scout", 10, 2);
+        let player = new Player("Scout", 10, 2, 3);
         player.takeDamage(12);
         console.log("bebouuuu  " + player.getHealth());
         expect(player.isAlive()).toBe(false);
     });
 
     it('should stay alive if not enough damage taken', function () {
-        let player = new Player("Scout", 10, 2);
+        let player = new Player("Scout", 10, 2, 3);
         player.takeDamage(5);
         expect(player.isAlive()).toBe(true);
+    });
+
+    it('should have the same name as given to the player', function () {
+        let player = new Player("Scout", 10, 2, 3);
+        expect(player.getName()).toBe("Scout");
+    });
+
+    it('should take less damage depending on defense', function (){
+        //given
+        let player = new Player("Scout", 10, 2, 3);
+        //when
+        player.takeDamage(5)
+        //then
+        expect(player.getHealth()).toBe(7)
+    })
+
+    it('should not take damage when defense superior than attack', function (){
+        let player = new Player("Scout", 10, 2, 3);
+        player.takeDamage(1)
+        expect(player.getHealth()).toBe(10)
+    })
+});
+
+describe('PLAYER INTERACTIONS', function () {
+    it('should be able to attack another player', function () {
+        let player1 = new Player("Tank", 15, 4, 2);
+        let player2 = new Player("Mage", 9, 1, 4);
+        player1.attackPlayer(player2);
+        expect(player2.getHealth()).toBe(8);
     });
 });
